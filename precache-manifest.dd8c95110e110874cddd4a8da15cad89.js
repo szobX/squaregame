3d7e91a3148c29565aa8f32df79dbca66d832f0d
Your Name <szoberpatryk@op.pl>
2019-12-02 16:39:37 +0100

    Updates

---
+++ precache-manifest.dd8c95110e110874cddd4a8da15cad89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d23bd9c9b5867d7aa3122e4a4d9aa8a",
+    "url": "/squaregame/index.html"
+  },
+  {
+    "revision": "836dfd2a2b31b8be3b17",
+    "url": "/squaregame/static/css/main.5e893738.chunk.css"
+  },
+  {
+    "revision": "c4c65fb8733389f5d0ed",
+    "url": "/squaregame/static/js/2.f090b940.chunk.js"
+  },
+  {
+    "revision": "836dfd2a2b31b8be3b17",
+    "url": "/squaregame/static/js/main.9e79882c.chunk.js"
+  },
+  {
+    "revision": "c65cff1d620ff047559a",
+    "url": "/squaregame/static/js/runtime-main.eda78ec0.js"
+  }
+]);
